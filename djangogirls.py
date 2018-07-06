bf8950b87880811903bb7e784a22df33a2e0c7e3
i.bauer <i.bauer@polylith.de>
2018-07-06 20:44:09 +0200

    Implemented nice waiting list algo

---
+++ djangogirls.py
@@ -0,0 +1,20 @@
+waiting_list = [
+    "Laura",
+    "Susanne",
+    "Karolin",
+    "Lisa",
+    "Andrea",
+    "Sabrine"
+]
+
+def print_position_in_waiting_list(name):
+    position = 1
+    for girl_name in waiting_list:
+        if girl_name == name:
+            print("Deine Position ist " + str(position))
+        position = position + 1
+
+print_position_in_waiting_list("Lisa")
+print_position_in_waiting_list("Karolin")
+
+print(1 + 1)
